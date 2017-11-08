d29d1cce9a0c8a4eeaa35c4b03cd011e31eac652
zkning <120716625@qq.com>
2017-11-08 18:25:07 +0800

    feat:添加请求request

---
+++ ubold-sm/src/main/java/com/ubold/admin/request/SqlDefineRequest.java
@@ -0,0 +1,79 @@
+package com.ubold.admin.request;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+import java.io.Serializable;
+
+/**
+ * Created by ningzuokun on 2017/11/8.
+ */
+public class SqlDefineRequest implements Serializable{
+
+    private String id;
+    @NotBlank(message = "sqlId不能为空")
+    private String sqlId;
+
+
+    @NotBlank(message = "sqlName不能为空")
+    private String sqlName;
+
+    @NotBlank(message = "selectSql不能为空")
+    private String selectSql;
+
+    @NotBlank(message = "masterTable不能为空")
+    private String masterTable;
+
+    /**
+     * 主表对应的ID
+     */
+    @NotBlank(message = "masterTableId不能为空")
+    private String masterTableId;
+
+    public String getSqlId() {
+        return sqlId;
+    }
+
+    public void setSqlId(String sqlId) {
+        this.sqlId = sqlId;
+    }
+
+    public String getSqlName() {
+        return sqlName;
+    }
+
+    public void setSqlName(String sqlName) {
+        this.sqlName = sqlName;
+    }
+
+    public String getSelectSql() {
+        return selectSql;
+    }
+
+    public void setSelectSql(String selectSql) {
+        this.selectSql = selectSql;
+    }
+
+    public String getMasterTable() {
+        return masterTable;
+    }
+
+    public void setMasterTable(String masterTable) {
+        this.masterTable = masterTable;
+    }
+
+    public String getMasterTableId() {
+        return masterTableId;
+    }
+
+    public void setMasterTableId(String masterTableId) {
+        this.masterTableId = masterTableId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
