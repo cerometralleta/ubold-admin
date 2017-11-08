5cbf3961c5d877816516002f1666624785b79f29
zkning <120716625@qq.com>
2017-11-08 18:33:19 +0800

    fix:添加

---
+++ ubold-rabc/src/main/java/com/ubold/admin/utils/SecurityUtils.java
@@ -0,0 +1,18 @@
+package com.ubold.admin.utils;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextImpl;
+
+/**
+ * Created by Kim on 2015/9/22.
+ */
+public class SecurityUtils {
+
+    public static void login(String username, String password) {
+        SecurityContextImpl securityContext = new SecurityContextImpl();
+        securityContext.setAuthentication(new UsernamePasswordAuthenticationToken(username, password, null));
+        SecurityContextHolder.setContext(securityContext);
+    }
+
+}
