92eefe2c14794acaac5271fc85ef1a2674f06545
zkning <120716625@qq.com>
2017-11-17 20:32:21 +0800

    fix:spring security

--- ubold-rabc/src/main/java/com/ubold/admin/config/WebSecurityConfiguration.java
+++ ubold-rabc/src/main/java/com/ubold/admin/config/WebSecurityConfiguration.java
@@ -1,8 +1,8 @@
 package com.ubold.admin.config;
 
 import com.alibaba.fastjson.JSONObject;
-import com.ubold.admin.filter.AuthenticationTokenProcessingFilter;
 import com.ubold.admin.filter.ServiceUnauthorizedEntryPoint;
+import com.ubold.admin.filter.UboldUsernamePasswordAuthenticationFilter;
 import com.ubold.admin.response.Response;
 import com.ubold.admin.service.impl.UboldAccessDecisionManager;
 import com.ubold.admin.service.impl.UboldAuthenticationProvider;
@@ -11,8 +11,8 @@ import org.apache.http.entity.ContentType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -44,8 +44,10 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     UboldAccessDecisionManager uboldAccessDecisionManager;
 
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
         http.headers().frameOptions().sameOrigin().disable()
                 .authorizeRequests()
                 .antMatchers("/api/permit/**").permitAll()
@@ -74,7 +76,9 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         .and().csrf().disable() //disable csrf
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)//无状态的web调用的stateless authentication
-        .and().exceptionHandling().authenticationEntryPoint(new ServiceUnauthorizedEntryPoint());
+        .and()
+                .addFilterAt(this.uboldUsernamePasswordAuthenticationFilter(),UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling().authenticationEntryPoint(new ServiceUnauthorizedEntryPoint());
     }
 
     @Override
@@ -85,4 +89,29 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         uboldAuthenticationProvider.setPasswordEncoder(new BCryptPasswordEncoder());
         auth.authenticationProvider(uboldAuthenticationProvider);
     }
+
+    @Bean
+    public UboldUsernamePasswordAuthenticationFilter uboldUsernamePasswordAuthenticationFilter() throws Exception {
+        UboldUsernamePasswordAuthenticationFilter filter = new UboldUsernamePasswordAuthenticationFilter();
+        filter.setAuthenticationManager(this.authenticationManager());
+        return filter;
+    }
+
+//    @Bean
+//    @Autowired
+//    public TokenStoreUserApprovalHandler userApprovalHandler(TokenStore tokenStore){
+//        TokenStoreUserApprovalHandler handler = new TokenStoreUserApprovalHandler();
+//        handler.setTokenStore(tokenStore);
+//        handler.setRequestFactory(new DefaultOAuth2RequestFactory(clientDetailsService));
+//        handler.setClientDetailsService(clientDetailsService);
+//        return handler;
+//    }
+//
+//    @Bean
+//    @Autowired
+//    public ApprovalStore approvalStore(TokenStore tokenStore) throws Exception {
+//        TokenApprovalStore store = new TokenApprovalStore();
+//        store.setTokenStore(tokenStore);
+//        return store;
+//    }
 }
--- ubold-web/src/main/java/com/ubold/admin/Application.java
+++ ubold-web/src/main/java/com/ubold/admin/Application.java
@@ -2,6 +2,7 @@ package com.ubold.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 /**
  * Created by ningzuokun on 2017/5/15.
