51d348f4a52a1d96e6962b97b100851fa17ad7a5
zkning <120716625@qq.com>
2017-12-16 19:46:00 +0800

    fix:@EnableOAuth2Sso开启 1.oauth2客户端需要WebSecurityConfigurerAdapter配置 2.contextPath不能为空 3.必须依赖spring-security-oauth2,spring-boot-starter-security

--- ubold-web/src/main/java/com/ubold/admin/Application.java
+++ ubold-web/src/main/java/com/ubold/admin/Application.java
@@ -1,16 +1,12 @@
 package com.ubold.admin;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Created by ningzuokun on 2017/5/15.
  */
-@EnableAutoConfiguration
-@Configuration
-@EnableOAuth2Sso
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
---
+++ ubold-web/src/main/java/com/ubold/admin/ClientSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.ubold.admin;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableOAuth2Sso
+@Configuration
+@Order(1)
+public class ClientSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/**")
+            .authorizeRequests()
+            .antMatchers("/", "/login**")
+            .permitAll()
+            .anyRequest()
+            .authenticated();
+    }
+}
