a041a26fce31d07c731ac0d9c7b893b7ea8b6637
zkning <120716625@qq.com>
2018-03-21 20:50:18 +0800

    fix:添加session校验

---
+++ ubold-rabc/src/main/java/com/ubold/admin/repository/ResourceRepository.java
@@ -0,0 +1,22 @@
+package com.ubold.admin.repository;
+
+import com.ubold.admin.domain.Resource;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Created by lenovo on 2017/11/11.
+ */
+@Repository
+public interface ResourceRepository extends JpaRepository<Resource, String> {
+
+
+    // 根据用户角色查询所有资源
+    @Query(value = "select trr.* from tb_rbac_resource trr where EXISTS (select ts.resource_id from (select trrrr.resource_id from tb_rbac_role_resource_relation trrrr \n" +
+            "where EXISTS (select trrur.role_id from tb_rbac_role_user_relation trrur where trrur.user_id = ?1 and trrur.role_id = trrrr.role_id)) ts where ts.resource_id = trr.id)  ", nativeQuery = true)
+    List<Resource> findResourceByRoleUserId(String userId);
+}
